9c057cced185d0ad6d6143be34624ca55e010c21
biubiu <dinochung2013@gmail.com>
2021-07-14 13:10:36 +0800

    update web crawler for company info on tianyancha web sit

--- webcrawler.py
+++ webcrawler.py
@@ -4,8 +4,8 @@ import time
 import random
 
 headers_tiayan = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64',
-           "Cookie": 'TYCID=f5148010de3011eba934a72619b177f2; ssuid=5676834200; _ga=GA1.2.1553602542.1625558746; tyc-user-phone=%5B%2213815862601%22%5D; creditGuide=1; bdHomeCount=11; aliyungf_tc=3bb9dd13be441906e924b27c401d177ec9ae9695434420e9fd4c1c498d14bb34; csrfToken=tyfu8mgjN3y-7FnZIP5O3ypg; bannerFlag=true; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1625742613,1625799249,1625816436,1625822426; jsid=https://www.tianyancha.com/?jsid=SEM-BAIDU-PP-TYC-000423&bd_vid=7879822996666481674&userid=31769301&query=%CC%EC%D1%DB&keywordid=281785359769&campaignid=158356455&groupid=5915607911&renqun_youhua=2828757; sensorsdata2015jssdkcross={"distinct_id":"13815862601","first_id":"17a7ad9f975cd1-05b94215a8ac9b-62725472-1764000-17a7ad9f97633","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":"未取到值_直接打开","$latest_referrer":""},"$device_id":"17a7ad9f975cd1-05b94215a8ac9b-62725472-1764000-17a7ad9f97633"}; cloud_token=12af6ba786ef4ca0bc1be50e964d2c74; searchSessionId=1625907150.37112275; relatedHumanSearchGraphId=11004087842; relatedHumanSearchGraphId.sig=VnnmczaIcXV3VpyslplQnmcl6oKIuhuRkJ5v5rFnUME; acw_tc=2f6fc12216260515404711695e14607715040b59ec3eb514effb33660ff8f8; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1626051537; _gid=GA1.2.1911186708.1626051538; tyc-user-info={"state":"0","vipManager":"0","mobile":"13815862601"}; tyc-user-info-save-time=1626051623252; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzgxNTg2MjYwMSIsImlhdCI6MTYyNjA1MTYyOCwiZXhwIjoxNjU3NTg3NjI4fQ.FTvcb2lLSQMG6BlanOKeqDZZZ9Iy6n6LVgAKUKlelzx31S4aLJhXmoy3wJpTw5DAdBSsypyIInjbjIvGk5c8Iw; creditGuide=1; CT_TYCID=a7c9a14f21d6437ea289070519ae2da8; RTYCID=87e14f4d0c004e27ba8fe08356e84ca3; bdHomeCount=11; sensorsdata2015jssdkcross={"distinct_id":"13815862601","first_id":"17a7ad9f975cd1-05b94215a8ac9b-62725472-1764000-17a7ad9f97633","props":{"$latest_traffic_source_type":"自然搜索流量","$latest_search_keyword":"未取到值","$latest_referrer":"https://www.baidu.com/baidu.php"},"$device_id":"17a7ad9f975cd1-05b94215a8ac9b-62725472-1764000-17a7ad9f97633"}; searchSessionId=1625816447.29016589; aliyungf_tc=3bb9dd13be441906e924b27c401d177ec9ae9695434420e9fd4c1c498d14bb34; csrfToken=tyfu8mgjN3y-7FnZIP5O3ypg; jsid=https://www.tianyancha.com/?jsid=SEM-BAIDU-PP-TYC-000423&bd_vid=9929724746923713584&userid=31769301&query=%CC%EC%D1%DB&keywordid=281785359769&campaignid=158356455&groupid=5915607911&renqun_youhua=2828757; bannerFlag=true; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1625742613,1625799249,1625816436,1625822426; acw_tc=781bad4f16258800238914614e78a3fee075c66dc46fd0c7da2088b99aa5b0; cloud_token=d5f8db964f074bbf8ed4f0b22a19c3be; cloud_utm=a83309ff2b094facbb0a9499c683862c; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1625880052; tyc-user-info={"state":"0","vipManager":"0","mobile":"13815862601"}; tyc-user-info-save-time=1625880224345; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzgxNTg2MjYwMSIsImlhdCI6MTYyNTg4MDIyOCwiZXhwIjoxNjU3NDE2MjI4fQ.0XxEylf6a7h32CTjkBUQTaO43TxPUzFqz6QiOD9P9In7Qm1oUspIzTnbLwxxMo_wDAnhNZTKgFEO-yxGJ_XzgQ'.encode('utf-8').decode('latin1')}
-login = {'user':'1381581','password':'xdpi7410.'}
+           "Cookie": 'TYCID=f5148010de3011eba934a72619b177f2; ssuid=5676834200; _ga=GA1.2.1553602542.1625558746; tyc-user-phone=%5B%2213815862601%22%5D; creditGuide=1; _gid=GA1.2.1911186708.1626051538; CT_TYCID=f6f0d701c9e34ac9983fae4099a34ffa; RTYCID=e189d9f161a64b03b03df27f65bf4956; acw_tc=2f6fc12f16261366123875643e417afe393f4734290ef8e9bd89652a7292ad; searchSessionId=1626136608.00652447; aliyungf_tc=cbdaeefd7fa4a7d18369e35303d56908d5a9245c2b166694d299ca72e9937f9d; csrfToken=zD8pR7Av8fw3FblI6JGePcDk; jsid=https://www.tianyancha.com/?jsid=SEM-BAIDU-PZ-SY-2021112-JRGW; sensorsdata2015jssdkcross={"distinct_id":"13815862601","first_id":"17a7ad9f975cd1-05b94215a8ac9b-62725472-1764000-17a7ad9f97633","props":{"$latest_traffic_source_type":"自然搜索流量","$latest_search_keyword":"天眼查","$latest_referrer":"https://www.baidu.com/other.php"},"$device_id":"17a7ad9f975cd1-05b94215a8ac9b-62725472-1764000-17a7ad9f97633"}; bdHomeCount=13; bannerFlag=true; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1625816436,1625822426,1626057947,1626137601; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1626137610; tyc-user-info={"state":"0","vipManager":"0","mobile":"13815862601"}; tyc-user-info-save-time=1626137627461; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzgxNTg2MjYwMSIsImlhdCI6MTYyNjEzNzYzMiwiZXhwIjoxNjU3NjczNjMyfQ.rAgVR8h2XDymJLyT-QDEA4vhuwU2qhjtp_U7GtLI6W-JM7XlhtRuC9d_NsSG3Y6OfY3LQ4VSIGazIXrBGuY_IA'.encode('utf-8').decode('latin1')}
+login = {'user':'13815862601','password':'xdpi7410.'}
 
 session_tianyan = requests.session()
 login_tianyan = session_tianyan.post('https://www.tianyancha.com/',data=login,headers=headers_tiayan)
@@ -20,8 +20,9 @@ def craylwer_tianyan(app):
     try: 
         #获取app在天眼中的链接
         #href = soup.find_all('a',{'class': 'name'})[0].get('href')
-        print(soup.find_all('a',{'class': 'name'}))
+        #print(soup.find_all('a',{'class': 'name'}))
         href = soup.find_all('a',{'class': 'name'})[0].get('href')
+        print(href)
         
         time.sleep(random.randint(10,20))
         html_app = session_tianyan.get(href,headers=headers_tiayan,timeout=10)
@@ -45,7 +46,8 @@ import csv
 apps = {}
 ff = open('C:\\Users\\Administrator\\.ssh\\github\exercise\\worklist_1.csv', 'r',encoding='gbk')
 csvfile = csv.reader(ff)
-for line in csvfile: 
+for line in csvfile:
+    if line[0] == '业务': continue 
     apps[line[0]] = {}
     apps[line[0]]['域名'] = line[1]
     apps[line[0]]['简介'] = line[2]
@@ -54,11 +56,12 @@ ff.close()
 
 cout = 0
 for app in apps: 
-    #if cout == 20: 
-    #    break
+    if cout == 50: 
+        break
     if apps[app]['域名']: 
         continue
-    time.sleep(random.randint(150,230))
+    print(f'_____{cout}')
+    time.sleep(random.randint(90,130))
     apps[app]['域名'], apps[app]['简介'] = craylwer_tianyan(app)
     print(app, ' ', apps[app]['域名'],' ', apps[app]['简介'] )
     cout += 1
@@ -69,4 +72,7 @@ for app in apps:
     print('writing ......................')
     csvwriter.writerow([app,str(apps[app]['域名']), str(apps[app]['简介'])])
 ff.close()
+
+
+
 
